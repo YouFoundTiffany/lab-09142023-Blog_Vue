07790a19b0270b46372f106274daf9c33dd6ad93
Camille Ivins <camillemivins@gmail.com>
2023-09-14 15:02:44 -0600

    whoops

--- src/components/BlogForm.vue
+++ src/components/BlogForm.vue
@@ -1,7 +1,7 @@
 <template>
     <form class="component row justify-content-between">
         <div class="mb-1 col-8">
-            <input type="text" class="form-control" type="text" placeholder="title">
+            <input type="text" class="form-control" placeholder="title">
         </div>
         <div class="mb-1 col-3">
 
