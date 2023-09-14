a73ae08b5709aa24aaa612d6f40377eaefd32d3a
Camille Ivins <camillemivins@gmail.com>
2023-09-14 16:49:11 -0600

    profile page

---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container"></div>
+</template>
+  
+<script>
+
+export default {
+    setup() {
+        return {}
+    },
+    components: { Login }
+}
+</script>
+  
+<style class="scss" scoped></style>
+  
