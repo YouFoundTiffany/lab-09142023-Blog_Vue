024da5cf0c02505340288aaa385b60333e158f67
Camille Ivins <camillemivins@gmail.com>
2023-09-14 15:36:31 -0600

    profile model up

---
+++ src/models/Profile.js
@@ -0,0 +1,35 @@
+export class Profile {
+    constructor(data) {
+        this.id = data.id
+        this.title = data.title
+        this.body = data.body
+        this.imgUrl = data.imgUrl
+        this.tags = data.tags
+        this.published = data.published
+        this.creator = data.creator
+
+    }
+}
+
+// "creator": {
+//     "_id": "64b8a6fc2bf6d261c3ee91fb",
+//     "subs": [
+//       "auth0|64b8a6fc4e7031ba54ffd813"
+//     ],
+//     "email": "iliketurtles@turtles.turtles",
+//     "name": "I Like Turtles",
+//     "picture": "https://media2.giphy.com/media/MwWfI1OnCe4yXPqxzv/200w.gif?cid=790b7611ettukgtyovbatitg14suc7bciu91izi0t2jvawy8&ep=v1_gifs_search&rid=200w.gif&ct=s",
+//     "bio": "Turtles are cool and coding is cool. A turtle that can code is super cool.",
+//     "coverImg": "https://previews.123rf.com/images/rubenreyes20/rubenreyes201905/rubenreyes20190500042/123147580-turtle-on-computer-with-keyboard-and-wireless-mouse-slow-internet-slow-processor.jpg",
+//     "github": "",
+//     "linkedin": "",
+//     "resume": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+//     "class": "Class of Summer 2023",
+//     "graduated": false,
+//     "createdAt": "2023-07-20T03:16:31.549Z",
+//     "updatedAt": "2023-07-23T22:37:39.689Z",
+//     "__v": 0,
+//     "id": "64b8a6fc2bf6d261c3ee91fb"
+//   },
+//   "id": "64b9e4ffeb84f17d842ed61a"
+// },
