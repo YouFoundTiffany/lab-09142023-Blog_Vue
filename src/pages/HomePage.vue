1c912ea886cc177256a5320d7a403caaa0e7cae8
tif <youfoundtiffany@gmail.com>
2023-09-14 14:26:33 -0600

    comm sync

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
-  </div>
+
+
+
+
 </template>
 
 <script>
+import { onMounted } from 'vue';
+import Pop from '../utils/Pop';
+import { logger } from '../utils/Logger';
+import { blogsService } from '../services/BlogsService.js';
 export default {
   setup() {
-    return {}
+    onMounted(() => {
+      getBlogs();
+
+
+    });
+    async function getBlogs() {
+      try {
+        await blogsService.getBlogs()
+        logger.log('📚')
+      } catch (error) {
+        Pop.error(error)
+      }
+
+    }
+
+    return {
+
+    }
   }
 }
 </script>
---
+++ src/services/BlogsService.js
@@ -0,0 +1,17 @@
+import { AppState } from '../AppState'
+import { Blog } from '../models/Blog.js'
+import { logger } from '../utils/Logger'
+import { api } from './AxiosService'
+
+
+class BlogsService {
+    async getBlogs() {
+        // AppState.blogs = []
+        const res = await api.get('api/blogs')
+        // logger.log(`gettin' them blogs`, res.data)
+        AppState.blogs = res.data.map(blog => new Blog(blog))
+    }
+
+}
+
+export const blogsService = new BlogsService()
