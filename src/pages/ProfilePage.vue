0852fe45aad34943ae03a889be7adb4b3bc111b4
Camille Ivins <camillemivins@gmail.com>
2023-09-14 16:53:46 -0600

    sync

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class="container"></div>
+    <div v-if="profile" class="container"></div>
+
+    <!-- NOTE don't render the profile info if there is none -->
+    <!-- Like when you reload, and your appstate activeProfile is null -->
+    <!-- if you get an error like 'Cannot read properties of null, reading ('picture')' this is your problem and this is the solution to it -->
+
+    <section class="row">
+
+
+    </section>
+
+    <section class="row">
+
+
+    </section>
 </template>
   
 <script>
