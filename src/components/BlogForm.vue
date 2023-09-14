49102b5742f9be3093d071b14344a42c2eb16f45
Camille Ivins <camillemivins@gmail.com>
2023-09-14 15:16:33 -0600

    More form details

--- src/components/BlogForm.vue
+++ src/components/BlogForm.vue
@@ -4,12 +4,26 @@
             <input type="text" class="form-control" placeholder="title">
         </div>
         <div class="mb-1 col-3">
+            <input type="text" class="form-control" placeholder="topic">
 
+        </div>
+
+        <div class="mb-1 col-12">
+            <input type="text" class="form-control" placeholder="Cover photo URL">
+
+        </div>
+        <div class="mb-1 col-12">
+            <label for="blog-text">Content</label>
+            <textarea id="blog-content" class="form-control" placeholder="Type content here" maxlength="10000"></textarea>
         </div>
     </form>
 </template>
 
+
+
 <script>
+import { AppState } from '../AppState.js';
+import
 
 </script>
 
